7abcce4de5b5ec1f8188d847e75fc57f23bfec9d
Davices <1253665718@qq.com>
2021-07-18 11:06:04 +0800

    添加项目文件。

---
+++ ExpressCode.IRepository/ITestRepository.cs
@@ -0,0 +1,16 @@
+﻿using Autofac.Extras.DynamicProxy;
+using ExpressCode.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressCode.IRepository
+{
+    [Intercept(typeof(AopTest))]
+    public interface ITestRepository
+    {
+        UserEntity test();
+    }
+}
