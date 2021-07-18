5b91bd6b3cb27dc89ed940873fe51e85e427d852
Davices <1253665718@qq.com>
2021-07-18 15:19:23 +0800

    T模型

---
+++ ExpressCode.Model/Admin/ModelEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressCode.Model
+{
+    public class ModelEntity:BaseEntity
+    {
+        public string Mname { get; set; }
+        public string Ssex { get; set; }
+        public string Aage { get; set; }
+
+       
+    }
+}
